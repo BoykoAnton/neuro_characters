9bb804a5b8c0db00f44467b9e93b7562360de71d
Anton Boyko <boyko.anton.mp@gmail.com>
2017-04-29 14:48:44 +0300

    First Commit
    Start project of neuro web on c#

---
+++ neuro_characters/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace neuro_characters
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Neuro WEB by A.B.";
+        }
+    }
+}
